f5c623a81676c216d83b3825489ff3369f6a9a64
Sakshi Soni <sakshi.soni@springrole.com>
2019-12-06 18:23:04 +0530

    Added condition for header

--- src/LaravelPdf/Pdf.php
+++ src/LaravelPdf/Pdf.php
@@ -44,14 +44,7 @@ class Pdf {
 		// please use the <title> tag.
 		$this->mpdf->SetTitle('Document');
 
-		$this->mpdf->AddPage('P', // L - landscape, P - portrait 
-		        '', '', '', '',
-		        '', // margin_left
-		        '', // margin right
-		       50, // margin top
-		       0, // margin bottom
-		        '', // margin header
-		        ''); // margin footer		
+				
 		$this->mpdf->SetAuthor        ( $this->getConfig('author') );
 		$this->mpdf->SetCreator       ( $this->getConfig('creator') );
 		$this->mpdf->SetSubject       ( $this->getConfig('subject') );
@@ -60,8 +53,16 @@ class Pdf {
 		
 		$this->mpdf->SetDisplayMode('fullpage');
 	
-	    $this->mpdf->SetHTMLHeader($this->config['header'],'',TRUE);
 	    if(isset($this->config['cols'])){
+	    	$this->mpdf->AddPage('P', // L - landscape, P - portrait 
+		        '', '', '', '',
+		        '', // margin_left
+		        '', // margin right
+		       50, // margin top
+		       0, // margin bottom
+		        '', // margin header
+		        ''); // margin footer
+	    	$this->mpdf->SetHTMLHeader($this->config['header'],'',TRUE);
 	    	$this->mpdf->SetColumns   ($this->config['cols'],'J');
 		}
 	  	$this->mpdf->showWatermarkText = true; 
